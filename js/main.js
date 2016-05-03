c37f6cf24c620dfe080282c4b74eed92a69cfa09
Tony Ren <renchk@foxmail.com>
2016-05-03 21:38:05 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,23 @@
- $(document).ready(function () {
+ $(document).ready(function() {
 
-    var showData = $('#show-data');
+     var showData = $('#show-data');
 
-    $.getJSON('data/Project.json', function (data) {
-      console.log(data);
+     $.getJSON('data/Project.json', function(data) {
+         console.log(data);
 
-      var items = data.items.map(function (item) {
-        return item.key + ': ' + item.value;
-      });
+         var items = data.items.map(function(item) {
+             console.log(item.key);
+             console.log(item.value);
+             return item.key + ': ' + item.value;
+         });
 
-      showData.empty();
+         showData.empty();
+         if (items.length) {
+             var content = '<li>' + items.join('</li><li>') + '</li>';
+             var list = $('<ul />').html(content);
+             showData.append(list);
+         }
+     });
 
-      if (items.length) {
-        var content = '<li>' + items.join('</li><li>') + '</li>';
-        var list = $('<ul />').html(content);
-        showData.append(list);
-      }
 
-
-  });
-  
-  
-});
+ });//End of ready 
